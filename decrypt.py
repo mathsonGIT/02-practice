abbd5127019624e76d6918adafd1ae9878dab75d
mathson <8804999@gmail.com>
2025-01-27 17:39:49 +0300

    third_task

--- decrypt.py
+++ decrypt.py
@@ -1,20 +1,29 @@
 import sys
-def decrypt():
-    cifr = sys.stdin.read()
-    decifr = cifr.split('..')
-    rezult = []
+def one_point_processing(text:str):
+    return(''.join(text.split('.')))
+
+def two_points_processing(text: str):
+    
+    decifr = text.split('..')
+    size = len(decifr)
     for i, _c in enumerate(decifr):
         print(_c)
-        if len(_c) == 0:
-            rezult[i-1] = (decifr[i-1][:-1])
-        else:
-            rezult.append(_c[:-1])
-    
-    return ''.join(rezult)
+        temp = _c
+        num_outliers = 1
+        while len(temp) == 0:
+            if (i+num_outliers) < size:
+                temp = decifr[i+num_outliers]
+            else:
+                break
+            num_outliers += 1
+        decifr[i-1] = decifr[i-1][:-num_outliers]
+ 
+    return one_point_processing(''.join(decifr))
 
 
             
     print(cifr.split('.'))
 
 if(__name__) == '__main__':
-    print(decrypt())
+    text = sys.stdin.read()
+    print(two_points_processing(text))
