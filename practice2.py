89840584e877e59820c864b39baea730ee8b4761
mathson <8804999@gmail.com>
2025-01-28 11:25:15 +0300

    flask_appliction_adding

--- practice2.py
+++ practice2.py
@@ -1,5 +1,7 @@
 from flask import Flask
 PATH = 'output.txt'
+
+
 def get_size_B(size: int):
     b_string = 'Сумма потребляемой памяти равна: '
     coeffs = [(1,'Б') , (1000, 'Кб'), (1000000, 'Мб') , (1000000000, 'Гб')]
@@ -9,19 +11,13 @@ def get_size_B(size: int):
             return(f'{b_string} {size // coeffs[i-1][0]} {coeffs[i-1][1]}')
     return(f'{b_string} {size // coeffs[-1][0]} {coeffs[-1][1]}')
 
+
 def get_summary_rss(path: str):
     with open(path, 'r') as file:
         rez = sum([int(line.split()[5]) for line in file.readlines()[1:]])
         return(get_size_B(rez))
 
 
-
-app = Flask(__name__)
-
-@app.route('/hello/<username>')
-def hello_world(username):
-    return(f'Hello World {username}')
-
 if(__name__) == '__main__':
-    #app.run(debug=True)
+
     print(get_summary_rss(PATH))
